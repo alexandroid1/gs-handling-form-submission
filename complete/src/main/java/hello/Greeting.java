18043f2eb1850f63b782bef1b3a0feba735777e5
Oleksandr <avpavlov96@gmail.com>
2016-05-29 00:28:07 +0300

    Greeting

--- complete/src/main/java/hello/Greeting.java
+++ complete/src/main/java/hello/Greeting.java
@@ -21,4 +21,11 @@ public class Greeting {
         this.content = content;
     }
 
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+
+
 }
